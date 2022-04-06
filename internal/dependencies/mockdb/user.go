8135a978f98a92d9672ca550ac59beff46e67843
Jacek Miloszewski <jacek.miloszewski@form3.tech>
2022-04-06 12:22:52 +0100

    zxc

--- internal/dependencies/mockdb/user.go
+++ internal/dependencies/mockdb/user.go
@@ -5,11 +5,13 @@ import (
 	"github.com/jmilosze/wfrp-hammergen-go/internal/config"
 	"github.com/jmilosze/wfrp-hammergen-go/internal/domain"
 	"golang.org/x/crypto/bcrypt"
+	"sync"
 )
 
+type userStore map[string]domain.User
+
 type UserService struct {
-	User1      domain.User
-	User2      domain.User
+	users      userStore
 	BcryptCost int
 }
 
@@ -19,27 +21,46 @@ func NewUserService(cfg *config.MockdbUserService) *UserService {
 
 	user1 := domain.User{Id: "0", Username: "User1", PasswordHash: user1password}
 	user2 := domain.User{Id: "1", Username: "User2", PasswordHash: user2password}
-	return &UserService{User1: user1, User2: user2, BcryptCost: cfg.BcryptCost}
+
+	users := userStore{user1.Id: user1, user2.Id: user2}
+
+	return &UserService{users: users, BcryptCost: cfg.BcryptCost}
 }
 
 func (s *UserService) FindUserById(id string) (*domain.User, error) {
-	if id == s.User1.Id {
-		return &s.User1, nil
-	} else if id == s.User2.Id {
-		return &s.User2, nil
+	var m sync.RWMutex
+
+	m.Lock()
+	user, ok := s.users[id]
+	m.Unlock()
+
+	if ok {
+		return &user, nil
 	} else {
 		return nil, errors.New("user not found")
 	}
 }
 
 func (s *UserService) FindUserByName(username string) (*domain.User, error) {
-	if username == s.User1.Username {
-		return &s.User1, nil
-	} else if username == s.User2.Username {
-		return &s.User2, nil
-	} else {
-		return nil, errors.New("user not found")
+	var m sync.RWMutex
+	var userFound *domain.User = nil
+
+	m.Lock()
+	for _, user := range s.users {
+		if user.Username == username {
+			userCopy := user
+			userCopy.PasswordHash = make([]byte, len(user.PasswordHash))
+			copy(userCopy.PasswordHash, user.PasswordHash)
+			userFound = &userCopy
+			break
+		}
 	}
+	m.Unlock()
+
+	if userFound != nil {
+		return userFound, nil
+	}
+	return nil, errors.New("user not found")
 }
 
 func (s *UserService) Authenticate(user domain.User, password string) bool {
